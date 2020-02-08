08f68b1227ae06acab9ad56062843d9af16dc389
HosanaAbollo <hosana.abollo2@gmail.com>
2020-02-08 17:26:10 +0100

    petite amelioration de la classe database

--- admin/includes/Database.php
+++ admin/includes/Database.php
@@ -13,12 +13,15 @@ class Database
     // Permet d'ouvrir la connection à la BDD
     public function open_db_connection()
     {
-        $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+       // $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-        // Vérifier si la connection est bien établie 
-        if(mysqli_connect_errno())
+       // Cet objet représente la connection à la BDD q 'on applique a la propriété conncetion 
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME); 
+
+        // Vérifier si la connection est bien établie avec l'OBJET 
+        if($this->connection->connect_errno)
         {
-            die("Connection a la base de donnée imposssible" . mysqli_error());
+            die("Connection a la base de donnée imposssible" . $this->connection->error);
         } 
 
     }
@@ -26,22 +29,29 @@ class Database
     // Permet d'envoyer des requêtes à la BDD
     public function query($sql){
 
-        $res = mysqli_query($this->connection, $sql);
+        $res = $this->connection->query($sql);
+
+        $this->confirm_query($res);  // Vérifier si résultat req avant retour resultat 
+        
         return $res;
     }
 
     // Permet de confirmer la requête 
     private function confirm_query($res){
         if(!$res){
-            die("Requête échouée");
+            die("Requête échouée" . $this->connection->error);
         }
     }
 
     // Permet d'échapper les caractères non valide
     public function escape_string($string)
     {
-        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        $escaped_string = $this->connection->real_escape_string($string);
         return $escaped_string;
+    }
+
+    public function the_insert_id(){
+        return $this->connection->insert_id;
     }
 
 
